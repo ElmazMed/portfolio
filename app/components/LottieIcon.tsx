5921a74f10c75bcd47ff9f39ebbf4affdb0e3258
ElmazMed <elmazgour.mohamed@gmail.com>
2025-08-04 03:18:27 +0300

    Created the first animated icon

---
+++ app/components/LottieIcon.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useRef, useEffect, useState } from 'react'
+import Lottie, { LottieRefCurrentProps } from 'lottie-react'
+import homeAnimation from '../../public/animations/home.json'
+
+export default function LottieIcon() {
+  const lottieRef = useRef<LottieRefCurrentProps>(null)
+  const [isHovered, setIsHovered] = useState(false)
+
+  const firstVisibleFrame = 60
+
+  useEffect(() => {
+    lottieRef.current?.goToAndStop(firstVisibleFrame, true)
+  }, [])
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+    lottieRef.current?.goToAndPlay(0, true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+    const animationDuration = lottieRef.current?.getDuration(true) || 100
+    lottieRef.current?.goToAndStop(animationDuration, true)
+  }
+
+  return (
+    <div
+      className="w-12 h-12 cursor-pointer transition-transform duration-300 hover:scale-110"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Lottie
+        lottieRef={lottieRef}
+        animationData={homeAnimation}
+        loop={false}
+        autoplay={false}
+      />
+    </div>
+  )
+}
